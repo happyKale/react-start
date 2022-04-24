b45176061895bec8773b46bc92d8ff489ab5e079
Yuna(김세연) <syk767@gmail.com>
2022-04-24 21:41:47 +0900

    Feat: add Switch, NotFoundScreen to AppRouter

--- front-end/src/app/routes/routes.tsx
+++ front-end/src/app/routes/routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import {
   HomeScreen,
   AdminScreen,
@@ -7,18 +7,20 @@ import {
   ProductsScreen,
   SignInScreen,
   SignUpScreen,
+  NotFoundScreen,
 } from "../screens";
 
 function AppRouter() {
   return (
-    <>
+    <Switch>
       <Route path="/" exact component={HomeScreen} />
       <Route path="/admin" component={AdminScreen} />
       <Route path="/carts" component={CartsScreen} />
       <Route path="/products/:id" component={ProductsScreen} />
       <Route path="/signin" component={SignInScreen} />
       <Route path="/signup" component={SignUpScreen} />
-    </>
+      <Route component={NotFoundScreen} />
+    </Switch>
   );
 }
 
--- front-end/src/app/screens/NotFound/NotFoundScreen.tsx
+++ front-end/src/app/screens/NotFound/NotFoundScreen.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 function NotFoundScreen() {
-  return <div>NotFound</div>;
+  // prop destruction
+
+  // lib hooks
+  let history = useHistory();
+
+  // state, ref, querystring hooks
+
+  // form hooks
+
+  // query hooks
+
+  // calculated values
+
+  // effects
+
+  // handlers
+
+  return (
+    <div>
+      NotFound
+      <button
+        onClick={() => {
+          history.goBack();
+        }}
+      >
+        뒤로가기
+      </button>
+    </div>
+  );
 }
 
 export { NotFoundScreen };
