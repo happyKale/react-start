81c9cbba5be68c6ccf1a4bb00f9be3cf8a044761
Yuna(김세연) <syk767@gmail.com>
2022-04-24 19:50:07 +0900

    Edit: rename sign-in, sign-up folder

--- front-end/src/app/screens/index.ts
+++ front-end/src/app/screens/index.ts
@@ -2,5 +2,5 @@ export * from "./admin";
 export * from "./carts";
 export * from "./home";
 export * from "./products";
-export * from "./sign-in";
-export * from "./sign-up";
+export * from "./signin";
+export * from "./signup";
