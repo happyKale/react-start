aa5e672cb97d598f71e66ad7f1e182f4c395fb6d
Yuna(김세연) <syk767@gmail.com>
2022-04-27 16:13:24 +0900

    Feat: add GridBox component

---
+++ front-end/src/app/components/GridBox/GridBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GridBox() {
+  return <div>GridBox</div>;
+}
+
+export { GridBox };
--- front-end/src/app/components/index.ts
+++ front-end/src/app/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./FlexBox";
 export * from "./Box";
+export * from "./GridBox";
