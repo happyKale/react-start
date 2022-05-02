e6a9e0f44924dad06c4924ecb401bbc58e432659
Yuna(김세연) <syk767@gmail.com>
2022-05-02 17:07:22 +0900

    Feat: add SignInScreen

--- front-end/src/app/screens/signin/SignInScreen.tsx
+++ front-end/src/app/screens/signin/SignInScreen.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Button, FlexBox, Input } from "../../components";
+import { Form } from "../../components";
 
 function SignInScreen() {
   // prop destruction
@@ -17,7 +19,47 @@ function SignInScreen() {
 
   // handlers
 
-  return <div>SignIn</div>;
+  return (
+    <Form
+      method="post"
+      width="400px"
+      height="200px"
+      border="2px solid red"
+      margin=" 50px auto"
+    >
+      로그인
+      <FlexBox
+        width="400px"
+        height="100px"
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        margin="0 auto"
+      >
+        <Input
+          type="text"
+          placeholder="아이디"
+          fontSize="1rem"
+          radius="4px 4px 0 0"
+          borderBottom="0px"
+        />
+        <Input
+          type="password"
+          placeholder="비밀번호"
+          fontSize="1rem"
+          radius="0 0 4px 4px"
+        />
+      </FlexBox>
+      <Button
+        backgroundColor="#a374db"
+        fontSize="1.2rem"
+        display="block"
+        margin="0 auto"
+      >
+        로그인
+      </Button>
+    </Form>
+  );
 }
 
 export { SignInScreen };
