14fc94b8eba5ead0c06169f2490b0661ae9c0ef5
Yuna(김세연) <syk767@gmail.com>
2022-05-04 23:23:09 +0900

    Feat: add redux-action, immer, ProductModel

--- front-end/src/app/redux/modules/product.ts
+++ front-end/src/app/redux/modules/product.ts
@@ -1,11 +1,21 @@
+import { createAction, handleActions } from "redux-actions";
+import { produce } from "immer";
+import { ProductModel } from "../../models/product-model";
+
 // 액션
-const LOAD = "product/LOAD";
-const CREATE = "product/CREATE";
-const UPDATE = "product/UPDATE";
-const DELETE = "product/DELETE";
+const LOAD = "LOAD";
+const CREATE = "CREATE";
+const UPDATE = "UPDATE";
+const REMOVE = "REMOVE";
+
+// 액션 생성 함수
+const load = createAction(LOAD, (product: ProductModel) => ({ product }));
+const create = createAction(CREATE, (product: ProductModel) => ({ product }));
+const update = createAction(UPDATE, (product: ProductModel) => ({ product }));
+const remove = createAction(REMOVE, (product: ProductModel) => ({ product }));
 
 // 초기 상태값
-const initialState = {
+const initialState: { list: Array<ProductModel> } = {
   // product 리스트를 가지고 있음.
   // models 폴더에 product 모델을 만들어야 될 듯???
   list: [
@@ -13,7 +23,7 @@ const initialState = {
       id: 1,
       name: "조던",
       image: "12345",
-      price: "100000",
+      price: 100000,
       brand: "나이키",
       category: "농구화",
       desc: "조던 농구화는 왕 멋지다.",
@@ -22,46 +32,25 @@ const initialState = {
   ],
 };
 
-// 액션 생성 함수
-export const loadProduct = (product: any) => {
-  return { type: LOAD, product };
-};
-export const createProduct = (product: any) => {
-  return { type: CREATE, product };
-};
-export const updateProduct = (product: any) => {
-  return { type: UPDATE, product };
-};
-export const deleteProduct = (product: { id: any }) => {
-  return { type: DELETE, product };
-};
-
 // 리듀서
 // 실질적으로 store에 들어가 있는 데이터를 변경하는 곳이죠!
-export default function reducer(
-  state = initialState,
-  action = { type: "", product: { id: 1 } }
-) {
-  switch (action.type) {
-    // do reducer stuff
-    case "product/LOAD": {
-      return state;
-    }
-    case "product/CREATE": {
-      const new_product_list = [...state.list, action.product];
-      return { list: new_product_list };
-    }
-    case "product/UPDATE": {
-      return state;
-    }
-    case "product/DELETE": {
-      const new_product_list = state.list.filter(
-        (item) => item.id !== action.product.id
-      );
-      console.log(action.product);
-      return { list: new_product_list };
-    }
-    default:
-      return state;
-  }
-}
+export default handleActions(
+  {
+    [LOAD]: (state, action) => produce(state, (draft) => {}),
+    [CREATE]: (state, action) =>
+      produce(state, (draft) => {
+        draft.list = [...state.list];
+      }),
+    [UPDATE]: (state, action) => produce(state, (draft) => {}),
+    [REMOVE]: (state, action) => produce(state, (draft) => {}),
+  },
+  initialState
+);
+
+const actionCreators = {
+  load,
+  create,
+  update,
+  remove,
+};
+export { actionCreators };
