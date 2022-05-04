586cddddd95a5ffe21049344d771ebeb9c51f061
Yuna(김세연) <syk767@gmail.com>
2022-05-04 15:06:24 +0900

    Feat: add ConnectedRouter

--- front-end/src/app/routes/routes.tsx
+++ front-end/src/app/routes/routes.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { ConnectedRouter } from "connected-react-router";
 import { Route, Switch } from "react-router-dom";
+import { history } from "../redux/configStore";
 import {
   HomeScreen,
   AdminScreen,
@@ -9,18 +11,24 @@ import {
   SignUpScreen,
   NotFoundScreen,
 } from "../screens";
+import { Header } from "../components";
 
 function AppRouter() {
   return (
-    <Switch>
-      <Route path="/" exact component={HomeScreen} />
-      <Route path="/admin" component={AdminScreen} />
-      <Route path="/carts" component={CartsScreen} />
-      <Route path="/products/:id" component={ProductsScreen} />
-      <Route path="/signin" component={SignInScreen} />
-      <Route path="/signup" component={SignUpScreen} />
-      <Route component={NotFoundScreen} />
-    </Switch>
+    // 원래 BrowserRouter와 Route를 사용해서 컴포넌트에 주입하던 history를
+    // ConnectedRouter를 사용해서 리덕스와 같은 history를 사용하도록 해줌!
+    <ConnectedRouter history={history}>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={HomeScreen} />
+        <Route path="/admin" component={AdminScreen} />
+        <Route path="/carts" component={CartsScreen} />
+        <Route path="/products/:id" component={ProductsScreen} />
+        <Route path="/signin" component={SignInScreen} />
+        <Route path="/signup" component={SignUpScreen} />
+        <Route component={NotFoundScreen} />
+      </Switch>
+    </ConnectedRouter>
   );
 }
 
