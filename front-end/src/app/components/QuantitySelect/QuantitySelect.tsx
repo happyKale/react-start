2be8332b973297244cdd671447cb6ddae2669d02
김세연 <yuna@gimseyeon-ui-MacBookPro.local>
2022-05-11 21:24:54 +0900

    Feat: add QuantitySelect component

--- front-end/src/app/components/QuantitySelect/QuantitySelect.tsx
+++ front-end/src/app/components/QuantitySelect/QuantitySelect.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button } from "../Button";
+import { FlexBox } from "../FlexBox";
 
-function QuantitySelect() {
+function QuantitySelect(props: { num: number }) {
   // prop destruction
+  const { num } = props;
 
   // lib hooks
 
   // state, ref, querystring hooks
+  const [quantity, setQuantity] = useState(0);
 
   // form hooks
 
@@ -14,9 +18,50 @@ function QuantitySelect() {
   // calculated values
 
   // effects
+  useEffect(() => {
+    setQuantity(num);
+  }, [num]);
 
   // handlers
-  return <div>QuantitySelect</div>;
+  const handlePlus = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setQuantity(quantity + 1);
+  };
+  const handleMinus = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (quantity !== 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  return (
+    <FlexBox
+      width="80px"
+      height="30px"
+      backgroundColor="#e4e4e4"
+      alignItems="center"
+      justifyContent="space-between"
+      radius="4px"
+    >
+      <Button
+        onClick={handlePlus}
+        color="black"
+        width="25px"
+        height="30px"
+        backgroundColor="#e4e4e4"
+      >
+        +
+      </Button>
+      {quantity}
+      <Button
+        onClick={handleMinus}
+        color="black"
+        width="25px"
+        height="30px"
+        backgroundColor="#e4e4e4"
+      >
+        -
+      </Button>
+    </FlexBox>
+  );
 }
 
 export { QuantitySelect };
