218254fdbfbd14cd407a9c299004ccc6c7eaf733
김세연 <yuna@gimseyeon-ui-MacBookPro.local>
2022-05-11 21:23:17 +0900

    Feat: add Path compoent to Header

--- front-end/src/app/components/Header/Header.tsx
+++ front-end/src/app/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { actionCreators as userActions } from "../../redux/modules/user";
+import { userActions } from "../../redux/modules/user";
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "../Button";
 import { useHistory } from "react-router-dom";
 import { RootState } from "../../redux/configStore";
 import { FlexBox } from "../FlexBox";
+import { Path } from "../Path";
 
 function Header() {
   // prop destruction
@@ -37,36 +38,59 @@ function Header() {
   };
 
   const handleLogout = (e: React.MouseEvent<HTMLButtonElement>) => {
-    dispatch(userActions.logOut());
+    dispatch(userActions.signOut());
     alert("로그아웃 되었습니다!");
   };
 
   console.log("헤더에서 로그인 상태: ", isLogin);
 
-  return loginState ? (
-    <FlexBox>
-      로그인 된 상태!{" "}
-      <Button
-        width="70px"
-        height="30px"
-        backgroundColor="#a374db"
-        onClick={handleLogout}
+  return (
+    <div>
+      <FlexBox
+        height="60px"
+        backgroundColor="lightgray"
+        alignItems="center"
+        justifyContent="space-between"
       >
-        로그아웃
-      </Button>
-    </FlexBox>
-  ) : (
-    <FlexBox>
-      로그인 안 된 상태!{" "}
-      <Button
-        width="120px"
-        height="30px"
-        backgroundColor="#a374db"
-        onClick={handleLogin}
-      >
-        로그인하러 가기
-      </Button>
-    </FlexBox>
+        <FlexBox
+          height="auto"
+          width="300px"
+          margin="0 0 0 50px"
+          justifyContent="space-between"
+        >
+          <Path url="/" name="홈" />
+          <Path url="/admin" name="제품등록" />
+          <Path url="/cart" name="장바구니" />
+          <Path url="/signin" name="로그인" />
+          <Path url="/signup" name="회원가입" />
+        </FlexBox>
+        {loginState ? (
+          <FlexBox width="250px" height="auto">
+            로그인 된 상태!{" "}
+            <Button
+              width="70px"
+              height="30px"
+              backgroundColor="#a374db"
+              onClick={handleLogout}
+            >
+              로그아웃
+            </Button>
+          </FlexBox>
+        ) : (
+          <FlexBox width="250px" height="auto">
+            로그인 안 된 상태!{" "}
+            <Button
+              width="120px"
+              height="30px"
+              backgroundColor="#a374db"
+              onClick={handleLogin}
+            >
+              로그인하러 가기
+            </Button>
+          </FlexBox>
+        )}
+      </FlexBox>
+    </div>
   );
 }
 
