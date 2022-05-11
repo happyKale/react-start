0f6721cf8b9c71bf795a4501d79b6cbcd3619825
김세연 <yuna@gimseyeon-ui-MacBookPro.local>
2022-05-11 21:16:39 +0900

    Feat: add api endpoint

--- front-end/src/app/configs/index.ts
+++ front-end/src/app/configs/index.ts
@@ -1,8 +1,2 @@
-// export const apiHost = process.env.HAULLA_API_HOST;
-export const apiHost = "http://localhost:4000/";
-
-// export const isDev = process.env.NODE_ENV !== 'production';
-
+export const apiHost = "http://54.242.247.77:8080/";
 export const API_ENDPOINT = apiHost;
-
-// export const STORAGE_TOKEN_KEY = 'back-office-token';
