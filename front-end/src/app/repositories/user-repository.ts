fbf060eddc71fdd635c2fd214966661b1085edc7
happyKale <syk767@gmail.com>
2022-05-12 14:59:40 +0900

    Feat: add data type

--- front-end/src/app/repositories/user-repository.ts
+++ front-end/src/app/repositories/user-repository.ts
@@ -1,28 +1,30 @@
-import { UserModel } from "../models/user-model";
 import { httpClient } from "../libs/http-client";
 
+type SignUpData = {
+  id: string;
+  pwd: string;
+  name: string;
+  phone: string;
+  email: string;
+  birth: string;
+  gender: "male" | "female" | "none";
+};
+
+type SignInData = {
+  id: string;
+  pwd: string;
+};
+
 const userRepository = {
-  getUser(role: "admin" | "customer", userId?: string) {
-    return httpClient.get<{ items: UserModel[] }>("/users").then((res) => {
+  signIn(userInfo: SignInData) {
+    return httpClient.post("/users/signin", { ...userInfo }).then((res) => {
       return res;
     });
   },
-  signIn(id: string, pwd: string) {
-    return httpClient.post("/signin", { id, pwd }).then((res) => {
+  signUp(userInfo: SignUpData) {
+    return httpClient.post("/users/signup", { ...userInfo }).then((res) => {
       return res;
     });
-  },
-  signUp(
-    id: string,
-    pwd: string,
-    pwdCheck: string,
-    name: string,
-    phone: string,
-    email: string,
-    birth: string,
-    gender: "male" | "female"
-  ) {
-    return httpClient.post("/signup", {});
   },
 };
 
