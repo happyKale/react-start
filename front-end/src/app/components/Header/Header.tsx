ef5079a7f46eb27ea4eb8c89ea88e7bc16e28d90
happyKale <syk767@gmail.com>
2022-05-16 21:49:02 +0900

    Feat: modify Header component as first designed

--- front-end/src/app/components/Header/Header.tsx
+++ front-end/src/app/components/Header/Header.tsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { userActions } from "../../redux/modules/user";
 import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
 import { Button } from "../Button";
-import { useHistory } from "react-router-dom";
 import { RootState } from "../../redux/configStore";
 import { FlexBox } from "../FlexBox";
 import { Path } from "../Path";
+import { Image } from "../Image";
+import logo from "../../../assets/logo.png";
+import search from "../../../assets/icon_search.png";
 
 function Header() {
   // prop destruction
 
   // lib hooks
   const dispatch = useDispatch();
-  const history = useHistory();
   const isLogin = useSelector((state: RootState) => state.user.is_login);
 
   // state, ref, querystring hooks
@@ -31,12 +33,6 @@ function Header() {
   }, [isLogin]);
 
   // handlers
-
-  // handlers
-  const handleLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
-    history.push("/signin");
-  };
-
   const handleLogout = (e: React.MouseEvent<HTMLButtonElement>) => {
     dispatch(userActions.signOut());
     alert("로그아웃 되었습니다!");
@@ -45,53 +41,98 @@ function Header() {
   console.log("헤더에서 로그인 상태: ", isLogin);
 
   return (
-    <div>
+    <FlexBox
+      direction="column"
+      height="200px"
+      border="1px solid black"
+      alignItems="center"
+    >
       <FlexBox
-        height="60px"
-        backgroundColor="lightgray"
-        alignItems="center"
+        margin="30px 0 0 0"
+        width="1000px"
+        height="auto"
         justifyContent="space-between"
       >
-        <FlexBox
-          height="auto"
-          width="300px"
-          margin="0 0 0 50px"
-          justifyContent="space-between"
-        >
-          <Path url="/" name="홈" />
-          <Path url="/admin" name="제품등록" />
-          <Path url="/cart" name="장바구니" />
-          <Path url="/signin" name="로그인" />
-          <Path url="/signup" name="회원가입" />
-        </FlexBox>
-        {loginState ? (
-          <FlexBox width="250px" height="auto">
-            로그인 된 상태!{" "}
-            <Button
-              width="70px"
-              height="30px"
-              backgroundColor="#a374db"
-              onClick={handleLogout}
-            >
-              로그아웃
-            </Button>
-          </FlexBox>
-        ) : (
-          <FlexBox width="250px" height="auto">
-            로그인 안 된 상태!{" "}
-            <Button
-              width="120px"
-              height="30px"
-              backgroundColor="#a374db"
-              onClick={handleLogin}
-            >
-              로그인하러 가기
-            </Button>
-          </FlexBox>
-        )}
+        <Image src={logo} width="154px" alt="Tend로고" />
+        <MenuList itemNum="4">
+          <MenuItem size="10px" weight="500" height="20px">
+            <Path to="/" children="MY PAGE" />
+          </MenuItem>
+          <MenuItem size="10px" weight="500" height="20px">
+            <Path to="/" children="MY HEART" />
+          </MenuItem>
+          <MenuItem size="10px" weight="500" height="20px">
+            <Path to="/" children="MY CART" />
+          </MenuItem>
+          <MenuItem size="10px" weight="500" height="20px">
+            {loginState ? (
+              <Button
+                width="70px"
+                height="30px"
+                backgroundColor="#a374db"
+                onClick={handleLogout}
+              >
+                로그아웃
+              </Button>
+            ) : (
+              <Path to="/signin" children="LOGIN" />
+            )}
+          </MenuItem>
+        </MenuList>
       </FlexBox>
-    </div>
+      <FlexBox
+        margin="10px 0 0 0"
+        width="1000px"
+        height="auto"
+        justifyContent="space-between"
+        alignItems="flex-end"
+      >
+        <MenuList itemNum="5">
+          <MenuItem size="39px" weight="600">
+            <Path to="/" children="SHOES" height="50px" />
+          </MenuItem>
+          <MenuItem size="39px" weight="600">
+            <Path to="/" children="HEADWEAR" height="50px" />
+          </MenuItem>
+          <MenuItem size="39px" weight="600">
+            <Path to="/" children="WE'ARE" height="50px" />
+          </MenuItem>
+          <MenuItem size="18px" weight="500">
+            <Path to="/" children="LOOKBOOK" />
+          </MenuItem>
+          <MenuItem size="18px" weight="500">
+            <Path to="/" children="EVENT" />
+          </MenuItem>
+        </MenuList>
+        <Image src={search} width="30px" height="30px" alt="검색아이콘" />
+      </FlexBox>
+    </FlexBox>
   );
 }
+
+const MenuList = styled.ul<{ itemNum: string }>`
+  display: grid;
+  ${(props) =>
+    props.itemNum
+      ? `grid-template-columns: repeat(${props.itemNum}, auto)`
+      : ""};
+  grid-template-rows: 60px 1fr;
+  margin: 0;
+  padding: 0;
+  column-gap: 40px;
+  list-style: none;
+`;
+
+const MenuItem = styled.li<{
+  size?: string;
+  weight?: string;
+  height?: string;
+}>`
+  display: flex;
+  align-items: flex-end;
+  ${(props) => (props.size ? `font-size: ${props.size}` : "")};
+  ${(props) => (props.weight ? `font-weight: ${props.weight}` : "")};
+  ${(props) => (props.height ? `height: ${props.height}` : "")};
+`;
 
 export { Header };
